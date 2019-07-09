7046083ffd1ffce6e1d30dc432851370ae8bcebc
Arul Prasad J <arulprasadj@outlook.com>
2019-07-09 17:35:48 +0530

    Create realtimeclock.js

---
+++ realtimeclock.js
@@ -0,0 +1,34 @@
+/*! Livedatetime ( real-time clock javascript ) -- Version: 1.2.0 -- Author: Arul Prasad J ('https://github.com/arulprasadj') */
+function live_date_time(id) {
+    date = new Date;
+    year = date.getFullYear();
+    month = date.getMonth();
+    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+    d = date.getDate();
+    day = date.getDay();
+    days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+    h = date.getHours();
+    var suffix = "AM";
+    if (h >= 12) {
+        suffix = "PM";
+        h = h - 12;
+        if (h < 10) {
+            h = "0" + h;
+        }
+    }
+    if (h == 0) {
+        h = 12;
+    }
+    m = date.getMinutes();
+    if (m < 10) {
+        m = "0" + m;
+    }
+    s = date.getSeconds();
+    if (s < 10) {
+        s = "0" + s;
+    }
+    result = '' + days[day] + ', ' + months[month] + ' ' + d + ' ' + year + ' ' + h + ':' + m + ':' + s + ' ' + suffix;
+    $("#" + id).html(result);
+    setTimeout('date_time("' + id + '");', '1000');
+    return true;
+}
